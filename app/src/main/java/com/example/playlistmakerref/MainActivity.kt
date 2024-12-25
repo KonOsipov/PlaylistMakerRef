14641a6d2a865153e942a9cd4a8a78130025e28e
KonOsipov <konstantin1410@yandex.ru>
2024-12-25 18:15:48 +0300

    3

--- app/src/main/java/com/example/playlistmakerref/MainActivity.kt
+++ app/src/main/java/com/example/playlistmakerref/MainActivity.kt
@@ -19,6 +19,7 @@ class MainActivity : ComponentActivity() {
             PlaylistMakerRefTheme {
                 // A surface container using the 'background' color from the theme
                 //
+                //3
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
